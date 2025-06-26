29a46cb759c6f7fe78cc5c4d5c4b2ed1ffdd4c8d
Lucas_Pasa <lucaspasagarcia29@gmail.com>
2025-06-26 11:09:44 +0100

    add validation form to the project

--- src/components/Popup/components/task/task.jsx
+++ src/components/Popup/components/task/task.jsx
@@ -7,51 +7,74 @@ export default function Task({ onAddTask }) {
   const [error, setError] = useState(false);
   const [message, setMessage] = useState("");
   const [typemessage, setTypeMessage] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
+  function isValid(e) {
+    if (e === "") {
+      setError(true);
+      setTypeMessage(true);
+      setMessage("Insira um valor válido");
+      setDisabled(true);
+    } else if (input.length < 5) {
+      setError(true);
+      setTypeMessage(true);
+      setMessage("Insira no minimo 5 caracteres");
+      setDisabled(true);
+    } else if (input.length > 50) {
+      setError(true);
+      setTypeMessage(true);
+      setMessage("Insira no maximo 50 caracteres");
+      setDisabled(true);
+    } else {
+      setError(false);
+      setDisabled(false);
+    }
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
     const form = e.target;
     const inputElement = form.elements["new-task"];
     if (input.trim() === "") {
-      setError(true);
-      setMessage("Insira um valor válido");
-      setTypeMessage(true);
       inputElement.value = "";
-    } else if (input.length < 5) {
-      setError(true);
-      setMessage("Insira no minimo 5 caracteres");
-      setTypeMessage(true);
-    } else if (input.length > 50) {
-      setError(true);
-      setMessage("Insira no maximo 50 caracteres");
-      setTypeMessage(true);
     } else {
       onAddTask(input);
       setInput("");
-      setError(true);
-      setMessage("Inserido com sucesso");
-      setTypeMessage(false);
       inputElement.value = "";
+      setError(true);
+      setTypeMessage(false);
+      setMessage("Inserido com sucesso");
+      setDisabled(true);
     }
   }
 
   return (
     <form className="form" onSubmit={handleSubmit} formNoValidate>
       <input
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) => {
+          setInput(e.target.value);
+          isValid(e.target.value);
+        }}
         name="new-task"
         className="form__input-task"
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
-        placeholder={isFocused ? "" : "Add new task..."}
+        placeholder={isFocused ? "" : "Add new task"}
       ></input>
       <span
-        className={`${error ? "input__error-visible" : ""}
-          ${typemessage ? "input__error" : "input__no-error "}`}
+        className={`${error ? "input__error-visible" : ""} ${
+          typemessage ? "input__error" : "input__no-error "
+        }`}
       >
         {error ? message : ""}
       </span>
-      <button className="form__task__button" type="submit">
+      <button
+        className={`form__task__button ${
+          disabled ? "form__task__button-disabled" : ""
+        }`}
+        type="submit"
+        disabled={disabled}
+      >
         <img src={plusSign} alt="PlusSign" className="form__task__image" />
       </button>
     </form>
